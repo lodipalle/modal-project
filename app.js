46f10ba87e58a716848a25884bf0fec040af9234
lodi palle <lodipalle@gmail.com>
2020-10-03 15:30:36 +1000

    uploaded hero with a higher quality image, updated CSS hero source and index.html

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector(".modal-btn");
-const modalOverlay = document.querySelector("modal-overlay");
+const modal = document.querySelector("modal-overlay");
 const closeBtn = document.querySelector(".close-tbn");
 
 modalBtn.addEventListener("click", function () {
-  modalOverlay.classList.add(".open-modal");
+  modal.classList.add(".open-modal");
 });
+
 closeBtn.addEventListener("click", function () {
-  modalOverlay.classList.remove(".open-modal");
+  modal.classList.remove(".open-modal");
 });
